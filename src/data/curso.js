ec3555a2a9dc10d4da2c3baed58718319f027484
LeoSan <cuenca623@gmail.com>
2026-04-02 15:11:48 -0600

    feat: add new Platzi Python and Django course certifications to list_cards

--- src/data/curso.js
+++ src/data/curso.js
@@ -1,4 +1,172 @@
 const list_cards = [
+    {
+        "anio": "",
+        "url": "/02_DesarrolloBackendPythonDjango_2025_Avanzado/02_CreacionCRUD/2025_diploma-python-practico_PLATZI.pdf",
+        "titulo": "2025 diploma python practico PLATZI",
+        "texto": "2025 diploma python practico PLATZI",
+        "enlace": "",
+        "empresa": ""
+    },
+    {
+        "anio": "",
+        "url": "/02_DesarrolloBackendPythonDjango_2025_Avanzado/01_CursoIntroduccionSeleniumPython/2025_Diploma-intro-selenium_PLATZI.pdf",
+        "titulo": "2025 Diploma intro selenium PLATZI",
+        "texto": "2025 Diploma intro selenium PLATZI",
+        "enlace": "",
+        "empresa": ""
+    },
+    {
+        "anio": "",
+        "url": "/02_DesarrolloBackendPythonDjango_2025_Avanzado/05_CursoEstadisticaComputacionalPython/2025_Diploma-programacion-estocastica_platzi.pdf",
+        "titulo": "2025 Diploma programacion estocastica platzi",
+        "texto": "2025 Diploma programacion estocastica platzi",
+        "enlace": "",
+        "empresa": ""
+    },
+    {
+        "anio": "",
+        "url": "/02_DesarrolloBackendPythonDjango_2025_Avanzado/04_ArquitecturaProyectosEntornosPyPI/2025_CursoPythonProfesionalArquitecturaPIP_PLATZI.pdf",
+        "titulo": "2025 CursoPythonProfesionalArquitecturaPIP PLATZI",
+        "texto": "2025 CursoPythonProfesionalArquitecturaPIP PLATZI",
+        "enlace": "",
+        "empresa": ""
+    },
+    {
+        "anio": "",
+        "url": "/02_DesarrolloBackendPythonDjango_2025_Avanzado/03_CursoPythonOrientadoObjetos/2025_Diploma-python-poo_PLATZI.pdf",
+        "titulo": "2025 Diploma python poo PLATZI",
+        "texto": "2025 Diploma python poo PLATZI",
+        "enlace": "",
+        "empresa": ""
+    },
+    {
+        "anio": "",
+        "url": "/02_DesarrolloBackendPythonDjango_2025_Avanzado/06_CursoEntornosVirtualesPEP8ManejoErrores/2025_CursoPythonEntornosvirtuales_PLATZI.pdf",
+        "titulo": "2025 CursoPythonEntornosvirtuales PLATZI",
+        "texto": "2025 CursoPythonEntornosvirtuales PLATZI",
+        "enlace": "",
+        "empresa": ""
+    },
+    {
+        "anio": "",
+        "url": "/01_Primeros_pasos_Python/2025_Diploma-Desarrollo-Backend-con-Python_PLATZI.pdf",
+        "titulo": "2025 Diploma Desarrollo Backend con Python PLATZI",
+        "texto": "2025 Diploma Desarrollo Backend con Python PLATZI",
+        "enlace": "",
+        "empresa": ""
+    },
+    {
+        "anio": "",
+        "url": "/01_Primeros_pasos_Python/2025_Diploma-backend-python_Django_PLATZI.pdf",
+        "titulo": "2025 Diploma backend python Django PLATZI",
+        "texto": "2025 Diploma backend python Django PLATZI",
+        "enlace": "",
+        "empresa": ""
+    },
+    {
+        "anio": "",
+        "url": "/01_Primeros_pasos_Python/12_CursoDespliegueAplicacionesPythonNube/2025_Diploma-Deploying-Python_PLATZI.pdf",
+        "titulo": "2025 Diploma Deploying Python PLATZI",
+        "texto": "2025 Diploma Deploying Python PLATZI",
+        "enlace": "",
+        "empresa": ""
+    },
+    {
+        "anio": "",
+        "url": "/01_Primeros_pasos_Python/01_CursoPython/diploma-gitgithub.pdf",
+        "titulo": "diploma gitgithub",
+        "texto": "diploma gitgithub",
+        "enlace": "",
+        "empresa": ""
+    },
+    {
+        "anio": "",
+        "url": "/01_Primeros_pasos_Python/01_CursoPython/2025_Diploma-python_PLATZI.pdf",
+        "titulo": "2025 Diploma python PLATZI",
+        "texto": "2025 Diploma python PLATZI",
+        "enlace": "",
+        "empresa": ""
+    },
+    {
+        "anio": "",
+        "url": "/01_Primeros_pasos_Python/06_Curso_Django/2025_Diploma-django_PLATZI.pdf",
+        "titulo": "2025 Diploma django PLATZI",
+        "texto": "2025 Diploma django PLATZI",
+        "enlace": "",
+        "empresa": ""
+    },
+    {
+        "anio": "",
+        "url": "/01_Primeros_pasos_Python/13_CursoEstructurasDatosLinealesPython/2025_Diploma-estructuras-datos-python_PLATZI.pdf",
+        "titulo": "2025 Diploma estructuras datos python PLATZI",
+        "texto": "2025 Diploma estructuras datos python PLATZI",
+        "enlace": "",
+        "empresa": ""
+    },
+    {
+        "anio": "",
+        "url": "/01_Primeros_pasos_Python/05_CursoPatronesDisenioSOLIDPython/2025_Diploma-solid-python_Platzi.pdf",
+        "titulo": "2025 Diploma solid python Platzi",
+        "texto": "2025 Diploma solid python Platzi",
+        "enlace": "",
+        "empresa": ""
+    },
+    {
+        "anio": "",
+        "url": "/01_Primeros_pasos_Python/07_Curso_Django_Rest_Framework/2025_diploma-django-rest-framework_platzi.pdf",
+        "titulo": "2025 diploma django rest framework platzi",
+        "texto": "2025 diploma django rest framework platzi",
+        "enlace": "",
+        "empresa": ""
+    },
+    {
+        "anio": "",
+        "url": "/01_Primeros_pasos_Python/04_CursoUnitTestingPython/2025_diploma-unit-testing-python_PLATZI.pdf",
+        "titulo": "2025 diploma unit testing python PLATZI",
+        "texto": "2025 diploma unit testing python PLATZI",
+        "enlace": "",
+        "empresa": ""
+    },
+    {
+        "anio": "",
+        "url": "/01_Primeros_pasos_Python/08_Curso_FastAPI/2025_DiplomaFastApi_PLATZI.pdf",
+        "titulo": "2025 DiplomaFastApi PLATZI",
+        "texto": "2025 DiplomaFastApi PLATZI",
+        "enlace": "",
+        "empresa": ""
+    },
+    {
+        "anio": "",
+        "url": "/01_Primeros_pasos_Python/03_Curso_Python_PIP_Entornos_Virtuales/2025_Diploma-python-pip_PLATZI.pdf",
+        "titulo": "2025 Diploma python pip PLATZI",
+        "texto": "2025 Diploma python pip PLATZI",
+        "enlace": "",
+        "empresa": ""
+    },
+    {
+        "anio": "",
+        "url": "/01_Primeros_pasos_Python/10_CursoComplejidadAlgoritmicaPythondd/2025_diploma-algoritmos-python_PLATZI.pdf",
+        "titulo": "2025 diploma algoritmos python PLATZI",
+        "texto": "2025 diploma algoritmos python PLATZI",
+        "enlace": "",
+        "empresa": ""
+    },
+    {
+        "anio": "",
+        "url": "/01_Primeros_pasos_Python/09_Curso_Flask/2025_Diploma-flask_platzi.pdf",
+        "titulo": "2025 Diploma flask platzi",
+        "texto": "2025 Diploma flask platzi",
+        "enlace": "",
+        "empresa": ""
+    },
+    {
+        "anio": "",
+        "url": "/01_Primeros_pasos_Python/02_Comprehensions_Funciones_Manejo_Errores/2025_Diploma-python-funciones_PLatzi.pdf",
+        "titulo": "2025 Diploma python funciones PLatzi",
+        "texto": "2025 Diploma python funciones PLatzi",
+        "enlace": "",
+        "empresa": ""
+    }
 ];
 
 export default list_cards;
